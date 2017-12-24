27765e02cbcef4145c7071618a5cf7cbde0c10b2
Maurus Cuelenaere <mcuelenaere@gmail.com>
2017-12-24 10:18:36 +0100

    Add 2017 day 24

---
+++ advent_of_code/2017/day24/part2.py
@@ -0,0 +1,22 @@
+from .shared import parse_text, create_combinations, calculate_bridge_strength
+
+
+def calculate(text: str) -> int:
+    components = parse_text(text)
+    combinations = tuple(create_combinations(components))
+    longest_bridge = max(len(bridge) for bridge in combinations)
+    max_strength = max(calculate_bridge_strength(bridge) for bridge in combinations if len(bridge) == longest_bridge)
+    return max_strength
+
+
+puzzle = """
+0/2
+2/2
+2/3
+3/4
+3/5
+0/1
+10/1
+9/10
+""".strip()
+assert calculate(puzzle) == 19
---
+++ advent_of_code/2017/day24/shared.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from typing import Set, Tuple, Dict, List, Iterator
+
+Component = Tuple[int, int]
+Bridge = List[Component]
+ComponentsByPort = Dict[int, Set[Tuple[int, Component]]]
+
+
+def parse_text(text: str) -> ComponentsByPort:
+    components = defaultdict(set)
+    for line in text.splitlines():
+        left, right = tuple(map(int, line.split('/', 2)))
+        components[left].add((right, (left, right)))
+        components[right].add((left, (left, right)))
+    return components
+
+
+def create_combinations(components: ComponentsByPort) -> Iterator[Bridge]:
+    def dfs(current_pin_cnt: int, bridge: Bridge):
+        for other_pin_cnt, comp in components[current_pin_cnt]:
+            if comp in bridge:
+                continue
+
+            yield bridge + [comp]
+            yield from dfs(other_pin_cnt, bridge + [comp])
+
+    yield from dfs(0, [])
+
+
+def calculate_bridge_strength(bridge: Bridge) -> int:
+    return sum(comp[0] + comp[1] for comp in bridge)
